6e99a3a8a60199a034d610389dad4020406eaddf
Bradley Odell <btodell@hotmail.com>
2021-12-28 20:26:25 -0800

    Implemented webpack config.

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,7 +1,89 @@
-import { Configuration } from "webpack";
+import { cpus } from "os";
+import { resolve } from "path";
+import { Configuration, EnvironmentPlugin } from "webpack";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import HtmlInlineScriptWebpackPlugin = require("html-inline-script-webpack-plugin");
 
 export = (env: unknown, argv: { mode: string }): Configuration => {
+    const dev: boolean = argv.mode == null || argv.mode === "development";
+
+    const inputPath = resolve(__dirname, "src");
+    const outputPath = resolve(__dirname, "dist");
+
+    const plugins: Configuration["plugins"] = [];
+
+    if (!dev) {
+        plugins.push(new EnvironmentPlugin({
+            NODE_ENV: "production",
+            DEBUG: false,
+        }));
+    }
+
+    const htmlWebpackPluginOptions: HtmlWebpackPlugin.Options = {
+        filename: resolve(outputPath, "index.html"),
+        inject: "body",
+    };
+    plugins.push(new HtmlWebpackPlugin(htmlWebpackPluginOptions));
+    if (!dev) {
+        plugins.push(new HtmlInlineScriptWebpackPlugin());
+    }
+
     return {
-        // TODO
+        mode: dev ? "development" : "production",
+        context: inputPath,
+        entry: [
+            "./index"
+        ],
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".scss"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    options: {
+                        configFile: resolve(inputPath, "tsconfig.json"),
+                        projectReferences: true,
+                    },
+                },
+                {
+                    test: /\.js$/,
+                    enforce: "pre",
+                    loader: "source-map-loader",
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader",
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: {
+                                    auto: true,
+                                    localIdentName: dev ? "[name]__[local]" : "[hash:base64]",
+                                    exportLocalsConvention: "camelCaseOnly",
+                                },
+                            },
+                        },
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: {
+                        loader: "url-loader",
+                    },
+                },
+            ],
+        },
+        plugins,
+        target: "web",
+        parallelism: cpus().length,
+        output: {
+            path: resolve(__dirname, "dist"),
+            publicPath: "/",
+            filename: dev ? "bundle.js?[fullhash]" : "bundle.js",
+        },
     };
 };
